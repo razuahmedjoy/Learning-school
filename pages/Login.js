6881d0ea1d63dbf77aca06ffc37cd83a27c1bfc7
srayo16 <srayosikder@gmail.com>
2022-06-19 01:25:35 +0600

    page setUp logIn

---
+++ pages/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h1>This is logIn page</h1>
+            <p>Under construction</p>
+        </div>
+    );
+};
+
+export default Login;
